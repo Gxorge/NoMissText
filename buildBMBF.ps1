0b45a1181e127224d5ae75c9f4b9de49280c4395
IsGabriellaCurious <georgehotten@outlook.com>
2021-02-06 19:05:17 +0000

    fixed name + added cover img

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libnomisstext.so", "./bmbfmod.json" -DestinationPath "./nomisstext_v2.0.0.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libnomisstext.so", "./bmbfmod.json", "./cover.jpg" -DestinationPath "./nomisstext_v2.0.0.zip" -Update
 }
